89f730a1cf4cc568bd3087622afc45a5b99f7cc1
Pulkit Gupta <pulkit30.bsr@gmail.com>
2021-09-02 17:32:04 +0530

    Day1

---
+++ Datatypes.js
@@ -0,0 +1,83 @@
+// VARIABLE DECLARATION
+/*
+
+1. let
+2. var
+3. const
+
+*/
+// when we declare any variable with let then we can reassign the variable and change it or modify anywhere in that code.
+//but not same for var
+//const cannot be changed or reassigned
+
+/*******************************************************/
+
+// DATATYPES IN JAVASCRIPT
+
+// 1. PRIMITIVE DATATYPE
+/*
+1.NUMBER
+        ANY NUMBER --- 1,23,2321,1233211,1232.22,13131.233131
+        ANY MATHEMATICAL OPERATION CAN BE DONE
+        -----  +,-,*,/,%....
+
+
+2.STRING
+    EXAMPLES --- "PULKIT GUPTA",'CLASS',`OBJECTS`,"JAVASCRIPT'S",`FUN'CHILL"ENJOY`
+
+
+3.BOOLEAN
+        ----- TRUE,FALSE
+
+4.UNDEFINED
+        ---- let MyName;
+            value of MyName is undefined
+
+
+5.NULL
+        ----let User=null;
+
+6.SYMBOLS
+        ---- let newSymbols=Symbol()
+        ----  Symbol()===Symbol()    //false
+        //exceptionally use
+
+7.BIGINT
+        ---- let BigInitNum=123456789012345678901234567890n;
+        n in the end shows that it is a bigint number
+        we can also define bigint num by
+        let BigIntNum2=BigInt('12345678901234567890n)
+        //exceptionally use
+
+*/
+// 2.REFERENCE DATATYPE
+/*
+1.ARRAYS
+    ---- example :- const Names=["pulkit","rohan","mohan"];
+                console.log(Names[0]);//'pulkit'
+                console.log(Names[2]);//'mohan'
+
+2.FUNCTIONS
+    ---- example :- function add(num1,num2){
+        return num1+num2;
+    }
+    add(2,2)//4
+    ES7
+    const add =(num1,num2)=>num1+num2;
+    add(2,2)//4
+
+
+3.OBJECTS
+    ----example :- const Names={
+        person1:"pulkit",
+        person2:"rohan",
+        person3:"mohan"
+    }
+    //this is the key value pairs
+    person1,person2,person3 -> keys
+    "pulkit,"rohan","mohan" -> values
+    console.log(Names.person1);//'pulkit'
+    console.log(Names.person3);//'mohan;
+
+*/
+/******************** NaN = Not a Number *********************/
---
+++ StringMethods.js
@@ -0,0 +1,45 @@
+/****************** STRING METHODS *******************/
+//declaring a string object
+// const StringObj = new String("Pulkit");
+// console.log(StringObj);
+//using the string object
+// console.log(StringObj[0]);
+// console.log(StringObj[5]);
+
+//declaring a string using string literal
+// const Name = "Pulkit";
+// console.log(Name);
+//to count the length of a string
+// const length = Name.length;
+// console.log(length);
+//to convert the string in uppercase
+// const UpperCase = Name.toUpperCase();
+// console.log(UpperCase);
+//to convert the string in LowerCase
+// const LowerCase = Name.toLowerCase();
+// console.log(LowerCase);
+//Finding the Index of the String letter
+// const IndexOfP = Name.indexOf("P");
+// console.log(IndexOfP); //0
+// const IndexOfU = Name.indexOf("u");
+// console.log(IndexOfU); //1
+// const IndexOfT = Name.indexOf("T");
+// console.log(IndexOfT); //-1//string not have any letter as T
+// we can also use
+// console.log(Name[0]); //P
+// console.log(Name[5]);
+//trim
+// const SpaceString = "      Pulkit is a Good Boy        "; //white spaces are count as string
+// console.log(SpaceString); //log string with spaces
+// console.log(SpaceString.trim()); //remove all white spaces form end and start
+// const newString = "this is a string";
+// string.slice(staringIndex,endingIndex);
+// console.log(newString.slice(0, 8));
+// console.log(newString.substr(3, 10)); //similar to slice()
+// console.log(newString.replace("this", "the"));
+// console.log(newString.concat(" ", "."));
+// console.log(newString.charAt(0));
+// console.log(newString.charCodeAt(9));
+// console.log(newString.split(" ")); //returns an array where each word is seprated by whitespace is new element
+// console.log(newString.includes("t")); //true
+// console.log(newString.includes("k")); //false
