22d43aed1be75be2f9b6f8a92028cb7e035b2e9f
Pulkit Gupta <pulkit30.bsr@gmail.com>
2021-09-22 17:09:52 +0530

    learned js Promises

--- JavaScript Misc/Js Promise.js
+++ JavaScript Misc/Js Promise.js
@@ -1,16 +1,70 @@
-//******** Javascript Promises
+//***** JAVASCRIPT PROMISE
+
 /*
-Promises in real-life express a trust between two or more persons and an assurance that a particular thing will surely happen. In javascript, a Promise is an object which ensures to produce a single value in the future (when required). Promise in javascript is used for managing and tackling asynchronous operations.
-
-
+Promises in real-life express a trust between two or more persons and an assurance that a particular thing will surely happen.
+In javascript, a Promise is an object which ensures to produce a single value in the future (when required).
+Promise in javascript is used for managing and tackling asynchronous operations.
 */
-var XMLHttpRequest = require("xhr2");
+/*
+Promise can be present in any 3 states:
+1.pending
+2.fulfilled
+3.reject
+4.resolve
+*/
+// syntax:
+/*
+let promise=new Promise((resolve,reject)=>{
+    if(error){
+        reject();
+    }else{
+        resolve();
+    }
+})
 
-function loadDoc() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function () {
-    document.getElementById("demo").innerHTML = this.responseText;
-  };
-  xhttp.open("GET", "sample.html", true);
-  xhttp.send();
-}
+promise.then(()=>{}).catch(error=>{})
+*/
+// example
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    error = false;
+    if (!error) {
+      resolve(1);
+    } else {
+      console.log("Rejected");
+      reject(0);
+    }
+  }, 2000);
+});
+
+// promise
+//   .then(() => {
+//     console.log("Function is executed successfully");
+//   })
+//   .catch((error) => {
+//     console.log("Bad request " + error);
+//   });
+//Promise Chaining
+/*
+when we use multiple then block with the promise, this is called Promise chaining
+*/
+promise
+  .then((data) => {
+    console.log(data);
+    return 1 * 2;
+  })
+  .then((data) => {
+    console.log(data);
+    return data * 2;
+  })
+  .then((data) => {
+    console.log(data);
+    return data * 2;
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
